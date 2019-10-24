ce63532eb17dd18573bb8192887706a0614a6561
bilinskask <bilinskask@example.com>
2019-10-24 19:53:51 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import './assets/style.scss';
-import Icon from './assets/img/giphy.gif';
+import './assets/img/logo1X.png';
+import './assets/img/flag1X.png';
+import './assets/img/crayon1X.png';
+import './assets/img/gears1X.png';
+import './assets/img/rocket1X.png';
+import './assets/img/monitor11X.png';
+import './assets/img/monitor21X.png';
+import './assets/img/monitor31X.png';
+import './assets/img/monitor41X.png';
+//import Icon from './assets/img/giphy.gif';
 
-const section = document.createElement('div');
-document.body.appendChild(section);
+// const section = document.createElement('div');
+// document.body.appendChild(section);
 
-const img = new Image()
-img.src = Icon
+// const img = new Image()
+// img.src = Icon
 
-document.querySelector("body").appendChild(img)
+// document.querySelector("body").appendChild(img)
 
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: "./src/index.js",
     mode: "development",
+    devtool: "inline-source-map",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "app.js"
@@ -41,7 +42,11 @@ module.exports = {
         ]
     },
     plugins:[
-        new HTMLWebpackPlugins(),
+        new HTMLWebpackPlugins({
+            title: "YetAnotherOne",
+            filename: "index.html",
+            template: "src/index.html"
+        }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin()
     ],
